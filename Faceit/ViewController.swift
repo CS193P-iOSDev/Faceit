18185cb7568e66ec988f3856b1a565be395a2293
gzh <hazel.gzh@gmail.com>
2017-05-31 09:15:04 +0800

    Initial commit.

---
+++ Faceit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by 郭秭含 on 2017/5/31.
+//  Copyright © 2017年 郭秭含. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
